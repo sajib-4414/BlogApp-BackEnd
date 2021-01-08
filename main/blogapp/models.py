d61dfcda48b8469c235a598eeb889937ce63a0f9
Shamsul Arefin <shamsul.arefin.sajib@gmail.com>
2021-01-08 02:19:29 -0600

    Blog post model is created, posts can be just addded through the admin panel now, will add api functionality soon

--- main/blogapp/models.py
+++ main/blogapp/models.py
@@ -1,3 +1,14 @@
+from django.contrib.auth.models import User
 from django.db import models
 
+
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.CharField(max_length=500)
+    author = models.ForeignKey(to=User, on_delete=models.CASCADE, null=True)
+
+    def __str__(self):
+        data = (self.title[:30] + '..') if len(self.title) > 30 else self.title
+        return data
+
