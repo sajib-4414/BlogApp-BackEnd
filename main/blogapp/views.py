8e5759f554164e78f16bc3fa1e1181eac21571f9
Shamsul Arefin <shamsul.arefin.sajib@gmail.com>
2021-01-08 23:42:02 -0600

    user retrieve api works with given user id

--- main/blogapp/views.py
+++ main/blogapp/views.py
@@ -1,11 +1,17 @@
 from django.contrib.auth import get_user_model
-User = get_user_model()
 from rest_framework import generics
 from rest_framework.permissions import AllowAny
 from main.blogapp.serializers import UserSerializer
+User = get_user_model()
 
 
 class UserCreateAPIView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = (AllowAny,)
+
+
+class UserRetrieveAPIView(generics.RetrieveAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
     permission_classes = (AllowAny,)
--- main/urls.py
+++ main/urls.py
@@ -20,5 +20,6 @@ from main.blogapp import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('users/', views.UserCreateAPIView.as_view()),
+    path('users/<int:pk>/', views.UserRetrieveAPIView.as_view()),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]
